fc91f54069f624c19b593adf77145e3e32bd8675
Tom Forbes <tom@tomforb.es>
2022-11-05 03:51:56 +0000

    {"name":"diqiu-zhifu-laizi-gaiyade-lingxun","version":"2022.11.5.0","file":"diqiu-zhifu-laizi-gaiyade-lingxun-2022.11.5.0.tar.gz","path":"diqiu-zhifu-laizi-gaiyade-lingxun/diqiu-zhifu-laizi-gaiyade-lingxun-2022.11.5.0.tar.gz"}

---
+++ diqiu-zhifu-laizi-gaiyade-lingxun/diqiu-zhifu-laizi-gaiyade-lingxun-2022.11.5.0.tar.gz/DiqiuZhifuLaiziGaiyadeLingxun/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""diqiu-zhifu-laizi-gaiyade-lingxun
+https://github.com/apachecn/diqiu-zhifu-laizi-gaiyade-lingxun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
