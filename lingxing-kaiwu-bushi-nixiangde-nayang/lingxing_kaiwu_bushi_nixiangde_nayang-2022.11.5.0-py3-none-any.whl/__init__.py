78c12bbd213e65136e54872c0ca719a1eb15a2b3
Tom Forbes <tom@tomforb.es>
2022-11-05 03:57:56 +0000

    {"name":"lingxing-kaiwu-bushi-nixiangde-nayang","version":"2022.11.5.0","file":"lingxing_kaiwu_bushi_nixiangde_nayang-2022.11.5.0-py3-none-any.whl","path":"lingxing-kaiwu-bushi-nixiangde-nayang/lingxing_kaiwu_bushi_nixiangde_nayang-2022.11.5.0-py3-none-any.whl"}

---
+++ lingxing-kaiwu-bushi-nixiangde-nayang/lingxing_kaiwu_bushi_nixiangde_nayang-2022.11.5.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingxing-kaiwu-bushi-nixiangde-nayang
+https://github.com/apachecn/lingxing-kaiwu-bushi-nixiangde-nayang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
