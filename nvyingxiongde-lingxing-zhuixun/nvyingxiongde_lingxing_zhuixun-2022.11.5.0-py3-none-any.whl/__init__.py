e989b9131048a55c62fb71b56caa6b844c90a8d7
Tom Forbes <tom@tomforb.es>
2022-11-05 04:00:54 +0000

    {"name":"nvyingxiongde-lingxing-zhuixun","version":"2022.11.5.0","file":"nvyingxiongde_lingxing_zhuixun-2022.11.5.0-py3-none-any.whl","path":"nvyingxiongde-lingxing-zhuixun/nvyingxiongde_lingxing_zhuixun-2022.11.5.0-py3-none-any.whl"}

---
+++ nvyingxiongde-lingxing-zhuixun/nvyingxiongde_lingxing_zhuixun-2022.11.5.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nvyingxiongde-lingxing-zhuixun
+https://github.com/apachecn/nvyingxiongde-lingxing-zhuixun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
