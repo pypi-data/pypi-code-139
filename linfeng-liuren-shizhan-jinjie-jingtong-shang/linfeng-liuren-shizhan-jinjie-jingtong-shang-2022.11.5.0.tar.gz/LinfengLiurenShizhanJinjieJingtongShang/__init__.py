3c7bb41abd763a453d7e2bfd08df5a8f644893bd
Tom Forbes <tom@tomforb.es>
2022-11-05 03:55:48 +0000

    {"name":"linfeng-liuren-shizhan-jinjie-jingtong-shang","version":"2022.11.5.0","file":"linfeng-liuren-shizhan-jinjie-jingtong-shang-2022.11.5.0.tar.gz","path":"linfeng-liuren-shizhan-jinjie-jingtong-shang/linfeng-liuren-shizhan-jinjie-jingtong-shang-2022.11.5.0.tar.gz"}

---
+++ linfeng-liuren-shizhan-jinjie-jingtong-shang/linfeng-liuren-shizhan-jinjie-jingtong-shang-2022.11.5.0.tar.gz/LinfengLiurenShizhanJinjieJingtongShang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""linfeng-liuren-shizhan-jinjie-jingtong-shang
+https://github.com/apachecn/linfeng-liuren-shizhan-jinjie-jingtong-shang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
