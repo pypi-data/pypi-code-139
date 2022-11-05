c6f982301e71c29c0a21d8482985946df0e3e74f
Tom Forbes <tom@tomforb.es>
2022-11-05 04:13:27 +0000

    {"name":"ziwei-doushu-mingli-zhenjie-sanbaili-zhong","version":"2022.11.5.0","file":"ziwei-doushu-mingli-zhenjie-sanbaili-zhong-2022.11.5.0.tar.gz","path":"ziwei-doushu-mingli-zhenjie-sanbaili-zhong/ziwei-doushu-mingli-zhenjie-sanbaili-zhong-2022.11.5.0.tar.gz"}

---
+++ ziwei-doushu-mingli-zhenjie-sanbaili-zhong/ziwei-doushu-mingli-zhenjie-sanbaili-zhong-2022.11.5.0.tar.gz/ZiweiDoushuMingliZhenjieSanbailiZhong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-doushu-mingli-zhenjie-sanbaili-zhong
+https://github.com/apachecn/ziwei-doushu-mingli-zhenjie-sanbaili-zhong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
