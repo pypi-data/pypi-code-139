1490d66090ee707481d78f348fecaf3a4f682aba
Tom Forbes <tom@tomforb.es>
2022-11-05 04:09:54 +0000

    {"name":"yueshuya-wenda-zhailu","version":"2022.11.5.0","file":"yueshuya-wenda-zhailu-2022.11.5.0.tar.gz","path":"yueshuya-wenda-zhailu/yueshuya-wenda-zhailu-2022.11.5.0.tar.gz"}

---
+++ yueshuya-wenda-zhailu/yueshuya-wenda-zhailu-2022.11.5.0.tar.gz/YueshuyaWendaZhailu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yueshuya-wenda-zhailu
+https://github.com/apachecn/yueshuya-wenda-zhailu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
