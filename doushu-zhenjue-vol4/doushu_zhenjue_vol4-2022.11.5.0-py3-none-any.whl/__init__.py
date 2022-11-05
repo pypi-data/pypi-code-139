4bdb520f569fb641df740ef1df11c7cb5c2876b4
Tom Forbes <tom@tomforb.es>
2022-11-05 03:52:22 +0000

    {"name":"doushu-zhenjue-vol4","version":"2022.11.5.0","file":"doushu_zhenjue_vol4-2022.11.5.0-py3-none-any.whl","path":"doushu-zhenjue-vol4/doushu_zhenjue_vol4-2022.11.5.0-py3-none-any.whl"}

---
+++ doushu-zhenjue-vol4/doushu_zhenjue_vol4-2022.11.5.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""doushu-zhenjue-vol4
+https://github.com/apachecn/doushu-zhenjue-vol4"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
