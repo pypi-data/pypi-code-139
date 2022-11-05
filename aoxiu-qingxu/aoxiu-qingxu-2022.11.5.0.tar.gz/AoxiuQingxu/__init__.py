b9482bd57d0bfb96b16995c3b95c9a2b21e9221f
Tom Forbes <tom@tomforb.es>
2022-11-05 03:49:53 +0000

    {"name":"aoxiu-qingxu","version":"2022.11.5.0","file":"aoxiu-qingxu-2022.11.5.0.tar.gz","path":"aoxiu-qingxu/aoxiu-qingxu-2022.11.5.0.tar.gz"}

---
+++ aoxiu-qingxu/aoxiu-qingxu-2022.11.5.0.tar.gz/AoxiuQingxu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-qingxu
+https://github.com/apachecn/aoxiu-qingxu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
