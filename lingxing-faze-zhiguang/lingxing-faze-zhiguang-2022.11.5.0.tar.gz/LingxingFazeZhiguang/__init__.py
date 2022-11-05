9057e7a664046d67ada52ecabe9b11c549a51c73
Tom Forbes <tom@tomforb.es>
2022-11-05 03:57:28 +0000

    {"name":"lingxing-faze-zhiguang","version":"2022.11.5.0","file":"lingxing-faze-zhiguang-2022.11.5.0.tar.gz","path":"lingxing-faze-zhiguang/lingxing-faze-zhiguang-2022.11.5.0.tar.gz"}

---
+++ lingxing-faze-zhiguang/lingxing-faze-zhiguang-2022.11.5.0.tar.gz/LingxingFazeZhiguang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingxing-faze-zhiguang
+https://github.com/apachecn/lingxing-faze-zhiguang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
