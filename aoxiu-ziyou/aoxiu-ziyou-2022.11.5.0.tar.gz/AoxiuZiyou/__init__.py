f5aed9692858ca06c88cf65a2f1262ddd665c8f9
Tom Forbes <tom@tomforb.es>
2022-11-05 03:50:58 +0000

    {"name":"aoxiu-ziyou","version":"2022.11.5.0","file":"aoxiu-ziyou-2022.11.5.0.tar.gz","path":"aoxiu-ziyou/aoxiu-ziyou-2022.11.5.0.tar.gz"}

---
+++ aoxiu-ziyou/aoxiu-ziyou-2022.11.5.0.tar.gz/AoxiuZiyou/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-ziyou
+https://github.com/apachecn/aoxiu-ziyou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
