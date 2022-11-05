9f757cc84414c98cf9d740e43992d19ba5351018
Tom Forbes <tom@tomforb.es>
2022-11-05 04:10:11 +0000

    {"name":"yueshuyade-chuandao-vol4","version":"2022.11.5.0","file":"yueshuyade_chuandao_vol4-2022.11.5.0-py3-none-any.whl","path":"yueshuyade-chuandao-vol4/yueshuyade_chuandao_vol4-2022.11.5.0-py3-none-any.whl"}

---
+++ yueshuyade-chuandao-vol4/yueshuyade_chuandao_vol4-2022.11.5.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yueshuyade-chuandao-vol4
+https://github.com/apachecn/yueshuyade-chuandao-vol4"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
