11830cb467e4fe6bd78ee1b238fb9cf683315a97
Tom Forbes <tom@tomforb.es>
2022-11-05 03:54:31 +0000

    {"name":"jinghua-mailun","version":"2022.11.5.0","file":"jinghua_mailun-2022.11.5.0-py3-none-any.whl","path":"jinghua-mailun/jinghua_mailun-2022.11.5.0-py3-none-any.whl"}

---
+++ jinghua-mailun/jinghua_mailun-2022.11.5.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jinghua-mailun
+https://github.com/apachecn/jinghua-mailun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
