ff9f31dfdd54cda60ad907b1eaa6b839c43249f0
Tom Forbes <tom@tomforb.es>
2022-11-05 03:58:45 +0000

    {"name":"lingxing-taijiao-shouce","version":"2022.11.5.0","file":"lingxing_taijiao_shouce-2022.11.5.0-py3-none-any.whl","path":"lingxing-taijiao-shouce/lingxing_taijiao_shouce-2022.11.5.0-py3-none-any.whl"}

---
+++ lingxing-taijiao-shouce/lingxing_taijiao_shouce-2022.11.5.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingxing-taijiao-shouce
+https://github.com/apachecn/lingxing-taijiao-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.5.0"
