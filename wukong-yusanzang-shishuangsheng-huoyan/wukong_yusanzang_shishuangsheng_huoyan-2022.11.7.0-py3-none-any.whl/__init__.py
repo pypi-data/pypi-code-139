b4d79ac6221a1f7d06875feb346e8006b557b48e
Tom Forbes <tom@tomforb.es>
2022-11-07 08:59:54 +0000

    {"name":"wukong-yusanzang-shishuangsheng-huoyan","version":"2022.11.7.0","file":"wukong_yusanzang_shishuangsheng_huoyan-2022.11.7.0-py3-none-any.whl","path":"wukong-yusanzang-shishuangsheng-huoyan/wukong_yusanzang_shishuangsheng_huoyan-2022.11.7.0-py3-none-any.whl"}

---
+++ wukong-yusanzang-shishuangsheng-huoyan/wukong_yusanzang_shishuangsheng_huoyan-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wukong-yusanzang-shishuangsheng-huoyan
+https://github.com/apachecn/wukong-yusanzang-shishuangsheng-huoyan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
