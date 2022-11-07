d5cc2669ddccecc4dc1a2114e80f4953e8cfac01
Tom Forbes <tom@tomforb.es>
2022-11-07 08:19:56 +0000

    {"name":"caiyun-fengshuixue","version":"2022.11.7.0","file":"caiyun_fengshuixue-2022.11.7.0-py3-none-any.whl","path":"caiyun-fengshuixue/caiyun_fengshuixue-2022.11.7.0-py3-none-any.whl"}

---
+++ caiyun-fengshuixue/caiyun_fengshuixue-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""caiyun-fengshuixue
+https://github.com/apachecn/caiyun-fengshuixue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
