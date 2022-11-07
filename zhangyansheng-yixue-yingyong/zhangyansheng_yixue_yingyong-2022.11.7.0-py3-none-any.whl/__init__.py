a0b0ed536e96234dd2c05717ac6b6a79d2ccff45
Tom Forbes <tom@tomforb.es>
2022-11-07 09:09:46 +0000

    {"name":"zhangyansheng-yixue-yingyong","version":"2022.11.7.0","file":"zhangyansheng_yixue_yingyong-2022.11.7.0-py3-none-any.whl","path":"zhangyansheng-yixue-yingyong/zhangyansheng_yixue_yingyong-2022.11.7.0-py3-none-any.whl"}

---
+++ zhangyansheng-yixue-yingyong/zhangyansheng_yixue_yingyong-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhangyansheng-yixue-yingyong
+https://github.com/apachecn/zhangyansheng-yixue-yingyong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
