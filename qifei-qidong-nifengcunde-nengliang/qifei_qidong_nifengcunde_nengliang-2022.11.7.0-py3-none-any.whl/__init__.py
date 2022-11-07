7df71f78da87eafeb77b20dab2f4190555d7a913
Tom Forbes <tom@tomforb.es>
2022-11-07 08:49:20 +0000

    {"name":"qifei-qidong-nifengcunde-nengliang","version":"2022.11.7.0","file":"qifei_qidong_nifengcunde_nengliang-2022.11.7.0-py3-none-any.whl","path":"qifei-qidong-nifengcunde-nengliang/qifei_qidong_nifengcunde_nengliang-2022.11.7.0-py3-none-any.whl"}

---
+++ qifei-qidong-nifengcunde-nengliang/qifei_qidong_nifengcunde_nengliang-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qifei-qidong-nifengcunde-nengliang
+https://github.com/apachecn/qifei-qidong-nifengcunde-nengliang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
