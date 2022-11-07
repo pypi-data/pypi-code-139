db7d5a1ecc5560a97880938cb46a94ecaf49a457
Tom Forbes <tom@tomforb.es>
2022-11-07 09:02:49 +0000

    {"name":"xinqiao-jiehe-shenxinde-nengliang-liaofa","version":"2022.11.7.0","file":"xinqiao-jiehe-shenxinde-nengliang-liaofa-2022.11.7.0.tar.gz","path":"xinqiao-jiehe-shenxinde-nengliang-liaofa/xinqiao-jiehe-shenxinde-nengliang-liaofa-2022.11.7.0.tar.gz"}

---
+++ xinqiao-jiehe-shenxinde-nengliang-liaofa/xinqiao-jiehe-shenxinde-nengliang-liaofa-2022.11.7.0.tar.gz/XinqiaoJieheShenxindeNengliangLiaofa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xinqiao-jiehe-shenxinde-nengliang-liaofa
+https://github.com/apachecn/xinqiao-jiehe-shenxinde-nengliang-liaofa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
