ddf865cae2c749aa9b2a8960f926bf51b0f85a31
Tom Forbes <tom@tomforb.es>
2022-11-07 08:43:52 +0000

    {"name":"maixiang-linghun-juexingde-diantang","version":"2022.11.7.0","file":"maixiang_linghun_juexingde_diantang-2022.11.7.0-py3-none-any.whl","path":"maixiang-linghun-juexingde-diantang/maixiang_linghun_juexingde_diantang-2022.11.7.0-py3-none-any.whl"}

---
+++ maixiang-linghun-juexingde-diantang/maixiang_linghun_juexingde_diantang-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""maixiang-linghun-juexingde-diantang
+https://github.com/apachecn/maixiang-linghun-juexingde-diantang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
