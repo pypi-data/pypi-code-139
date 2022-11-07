6ed6a6a4ec68bb88e4a7a3fdcfb3593aacc548ca
Tom Forbes <tom@tomforb.es>
2022-11-07 08:50:34 +0000

    {"name":"qiucai-fengshui-miji","version":"2022.11.7.0","file":"qiucai-fengshui-miji-2022.11.7.0.tar.gz","path":"qiucai-fengshui-miji/qiucai-fengshui-miji-2022.11.7.0.tar.gz"}

---
+++ qiucai-fengshui-miji/qiucai-fengshui-miji-2022.11.7.0.tar.gz/QiucaiFengshuiMiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qiucai-fengshui-miji
+https://github.com/apachecn/qiucai-fengshui-miji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
