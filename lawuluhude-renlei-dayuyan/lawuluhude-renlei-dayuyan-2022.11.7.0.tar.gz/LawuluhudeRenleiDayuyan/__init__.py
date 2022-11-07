a40bd8325fbd9e183b50da8f4141121e1b9557bc
Tom Forbes <tom@tomforb.es>
2022-11-07 08:38:52 +0000

    {"name":"lawuluhude-renlei-dayuyan","version":"2022.11.7.0","file":"lawuluhude-renlei-dayuyan-2022.11.7.0.tar.gz","path":"lawuluhude-renlei-dayuyan/lawuluhude-renlei-dayuyan-2022.11.7.0.tar.gz"}

---
+++ lawuluhude-renlei-dayuyan/lawuluhude-renlei-dayuyan-2022.11.7.0.tar.gz/LawuluhudeRenleiDayuyan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lawuluhude-renlei-dayuyan
+https://github.com/apachecn/lawuluhude-renlei-dayuyan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
