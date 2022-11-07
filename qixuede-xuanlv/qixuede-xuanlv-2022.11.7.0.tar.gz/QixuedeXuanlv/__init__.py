889a47541f4f2a2732c8c197ca7eb70aa97f53e4
Tom Forbes <tom@tomforb.es>
2022-11-07 08:50:58 +0000

    {"name":"qixuede-xuanlv","version":"2022.11.7.0","file":"qixuede-xuanlv-2022.11.7.0.tar.gz","path":"qixuede-xuanlv/qixuede-xuanlv-2022.11.7.0.tar.gz"}

---
+++ qixuede-xuanlv/qixuede-xuanlv-2022.11.7.0.tar.gz/QixuedeXuanlv/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qixuede-xuanlv
+https://github.com/apachecn/qixuede-xuanlv"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
