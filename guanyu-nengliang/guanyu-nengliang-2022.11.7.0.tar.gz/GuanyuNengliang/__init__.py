077cbd15e15323aa99c49d8894fab60c7fbfc0ab
Tom Forbes <tom@tomforb.es>
2022-11-07 08:28:46 +0000

    {"name":"guanyu-nengliang","version":"2022.11.7.0","file":"guanyu-nengliang-2022.11.7.0.tar.gz","path":"guanyu-nengliang/guanyu-nengliang-2022.11.7.0.tar.gz"}

---
+++ guanyu-nengliang/guanyu-nengliang-2022.11.7.0.tar.gz/GuanyuNengliang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""guanyu-nengliang
+https://github.com/apachecn/guanyu-nengliang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
