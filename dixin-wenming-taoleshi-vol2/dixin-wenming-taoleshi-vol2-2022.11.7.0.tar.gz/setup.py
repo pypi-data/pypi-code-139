dd57282fd069683e556e3193e3eeb4e92aa17f28
Tom Forbes <tom@tomforb.es>
2022-11-07 08:25:45 +0000

    {"name":"dixin-wenming-taoleshi-vol2","version":"2022.11.7.0","file":"dixin-wenming-taoleshi-vol2-2022.11.7.0.tar.gz","path":"dixin-wenming-taoleshi-vol2/dixin-wenming-taoleshi-vol2-2022.11.7.0.tar.gz"}

---
+++ dixin-wenming-taoleshi-vol2/dixin-wenming-taoleshi-vol2-2022.11.7.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import DixinWenmingTaoleshiVol2
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('DixinWenmingTaoleshiVol2'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="dixin-wenming-taoleshi-vol2",
+    version=DixinWenmingTaoleshiVol2.__version__,
+    url="https://github.com/apachecn/dixin-wenming-taoleshi-vol2",
+    author=DixinWenmingTaoleshiVol2.__author__,
+    author_email=DixinWenmingTaoleshiVol2.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="地心文明桃乐市2人类扬升的光启之道",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "dixin-wenming-taoleshi-vol2=DixinWenmingTaoleshiVol2.__main__:main",
+            "DixinWenmingTaoleshiVol2=DixinWenmingTaoleshiVol2.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
