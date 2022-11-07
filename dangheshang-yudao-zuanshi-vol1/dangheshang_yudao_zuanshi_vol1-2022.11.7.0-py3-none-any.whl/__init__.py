f55f074feda372dc30a151534f3466a66a3119d9
Tom Forbes <tom@tomforb.es>
2022-11-07 08:22:07 +0000

    {"name":"dangheshang-yudao-zuanshi-vol1","version":"2022.11.7.0","file":"dangheshang_yudao_zuanshi_vol1-2022.11.7.0-py3-none-any.whl","path":"dangheshang-yudao-zuanshi-vol1/dangheshang_yudao_zuanshi_vol1-2022.11.7.0-py3-none-any.whl"}

---
+++ dangheshang-yudao-zuanshi-vol1/dangheshang_yudao_zuanshi_vol1-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dangheshang-yudao-zuanshi-vol1
+https://github.com/apachecn/dangheshang-yudao-zuanshi-vol1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
