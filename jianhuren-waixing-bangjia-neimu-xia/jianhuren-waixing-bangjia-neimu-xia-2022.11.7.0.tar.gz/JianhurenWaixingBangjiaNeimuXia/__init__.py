0d375be6cbbc896c5f060c26f7e7e55ca08192fd
Tom Forbes <tom@tomforb.es>
2022-11-07 08:33:45 +0000

    {"name":"jianhuren-waixing-bangjia-neimu-xia","version":"2022.11.7.0","file":"jianhuren-waixing-bangjia-neimu-xia-2022.11.7.0.tar.gz","path":"jianhuren-waixing-bangjia-neimu-xia/jianhuren-waixing-bangjia-neimu-xia-2022.11.7.0.tar.gz"}

---
+++ jianhuren-waixing-bangjia-neimu-xia/jianhuren-waixing-bangjia-neimu-xia-2022.11.7.0.tar.gz/JianhurenWaixingBangjiaNeimuXia/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jianhuren-waixing-bangjia-neimu-xia
+https://github.com/apachecn/jianhuren-waixing-bangjia-neimu-xia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
