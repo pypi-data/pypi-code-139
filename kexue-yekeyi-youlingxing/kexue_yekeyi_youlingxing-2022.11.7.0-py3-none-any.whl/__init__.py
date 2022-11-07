9eb7beb59111468e68eaeb509ebb5e2a1cfa5edb
Tom Forbes <tom@tomforb.es>
2022-11-07 08:38:25 +0000

    {"name":"kexue-yekeyi-youlingxing","version":"2022.11.7.0","file":"kexue_yekeyi_youlingxing-2022.11.7.0-py3-none-any.whl","path":"kexue-yekeyi-youlingxing/kexue_yekeyi_youlingxing-2022.11.7.0-py3-none-any.whl"}

---
+++ kexue-yekeyi-youlingxing/kexue_yekeyi_youlingxing-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kexue-yekeyi-youlingxing
+https://github.com/apachecn/kexue-yekeyi-youlingxing"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
