ee7294bccc7b61a9c559917b46dd12302a0cde82
Tom Forbes <tom@tomforb.es>
2022-11-07 09:04:14 +0000

    {"name":"yiliao-lingmei","version":"2022.11.7.0","file":"yiliao_lingmei-2022.11.7.0-py3-none-any.whl","path":"yiliao-lingmei/yiliao_lingmei-2022.11.7.0-py3-none-any.whl"}

---
+++ yiliao-lingmei/yiliao_lingmei-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yiliao-lingmei
+https://github.com/apachecn/yiliao-lingmei"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
