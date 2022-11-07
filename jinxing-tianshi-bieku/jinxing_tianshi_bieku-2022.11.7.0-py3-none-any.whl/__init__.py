3d8592619d308dc83039923c520e40f361a1e76c
Tom Forbes <tom@tomforb.es>
2022-11-07 08:36:43 +0000

    {"name":"jinxing-tianshi-bieku","version":"2022.11.7.0","file":"jinxing_tianshi_bieku-2022.11.7.0-py3-none-any.whl","path":"jinxing-tianshi-bieku/jinxing_tianshi_bieku-2022.11.7.0-py3-none-any.whl"}

---
+++ jinxing-tianshi-bieku/jinxing_tianshi_bieku-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jinxing-tianshi-bieku
+https://github.com/apachecn/jinxing-tianshi-bieku"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
