ffea39a2589785f65a733adcec3eda959640d5f9
Tom Forbes <tom@tomforb.es>
2022-11-07 08:29:01 +0000

    {"name":"hailun-pama-jiuxing-renhe-shengjing","version":"2022.11.7.0","file":"hailun-pama-jiuxing-renhe-shengjing-2022.11.7.0.tar.gz","path":"hailun-pama-jiuxing-renhe-shengjing/hailun-pama-jiuxing-renhe-shengjing-2022.11.7.0.tar.gz"}

---
+++ hailun-pama-jiuxing-renhe-shengjing/hailun-pama-jiuxing-renhe-shengjing-2022.11.7.0.tar.gz/HailunPamaJiuxingRenheShengjing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hailun-pama-jiuxing-renhe-shengjing
+https://github.com/apachecn/hailun-pama-jiuxing-renhe-shengjing"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
