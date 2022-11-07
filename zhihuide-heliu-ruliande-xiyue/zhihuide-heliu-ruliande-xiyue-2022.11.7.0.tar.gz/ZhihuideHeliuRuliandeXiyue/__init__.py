6c2445a7101fe664bc35fa162bc611b6cf88c212
Tom Forbes <tom@tomforb.es>
2022-11-07 09:10:34 +0000

    {"name":"zhihuide-heliu-ruliande-xiyue","version":"2022.11.7.0","file":"zhihuide-heliu-ruliande-xiyue-2022.11.7.0.tar.gz","path":"zhihuide-heliu-ruliande-xiyue/zhihuide-heliu-ruliande-xiyue-2022.11.7.0.tar.gz"}

---
+++ zhihuide-heliu-ruliande-xiyue/zhihuide-heliu-ruliande-xiyue-2022.11.7.0.tar.gz/ZhihuideHeliuRuliandeXiyue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhihuide-heliu-ruliande-xiyue
+https://github.com/apachecn/zhihuide-heliu-ruliande-xiyue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
