14389accd0b0c258e0b7f1a5fd013fd2dcbcb3cd
Tom Forbes <tom@tomforb.es>
2022-11-07 08:41:43 +0000

    {"name":"lingtongling-boshi-shijianbu","version":"2022.11.7.0","file":"lingtongling_boshi_shijianbu-2022.11.7.0-py3-none-any.whl","path":"lingtongling-boshi-shijianbu/lingtongling_boshi_shijianbu-2022.11.7.0-py3-none-any.whl"}

---
+++ lingtongling-boshi-shijianbu/lingtongling_boshi_shijianbu-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingtongling-boshi-shijianbu
+https://github.com/apachecn/lingtongling-boshi-shijianbu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
