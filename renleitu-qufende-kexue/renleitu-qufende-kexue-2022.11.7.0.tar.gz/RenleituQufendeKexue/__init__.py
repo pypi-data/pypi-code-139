e1bfcd9d9fc64ce37f8d27c884f9e6cc86e54aec
Tom Forbes <tom@tomforb.es>
2022-11-07 08:53:46 +0000

    {"name":"renleitu-qufende-kexue","version":"2022.11.7.0","file":"renleitu-qufende-kexue-2022.11.7.0.tar.gz","path":"renleitu-qufende-kexue/renleitu-qufende-kexue-2022.11.7.0.tar.gz"}

---
+++ renleitu-qufende-kexue/renleitu-qufende-kexue-2022.11.7.0.tar.gz/RenleituQufendeKexue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""renleitu-qufende-kexue
+https://github.com/apachecn/renleitu-qufende-kexue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
