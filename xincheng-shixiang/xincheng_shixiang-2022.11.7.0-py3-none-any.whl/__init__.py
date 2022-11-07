a32949b747a6debddd391ecaff6be69427b39528
Tom Forbes <tom@tomforb.es>
2022-11-07 09:00:20 +0000

    {"name":"xincheng-shixiang","version":"2022.11.7.0","file":"xincheng_shixiang-2022.11.7.0-py3-none-any.whl","path":"xincheng-shixiang/xincheng_shixiang-2022.11.7.0-py3-none-any.whl"}

---
+++ xincheng-shixiang/xincheng_shixiang-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xincheng-shixiang
+https://github.com/apachecn/xincheng-shixiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
