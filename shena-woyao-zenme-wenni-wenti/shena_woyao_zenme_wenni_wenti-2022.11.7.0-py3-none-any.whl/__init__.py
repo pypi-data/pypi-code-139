046c59ca7154e0a1b4397a5eac06385e58f4101b
Tom Forbes <tom@tomforb.es>
2022-11-07 08:56:13 +0000

    {"name":"shena-woyao-zenme-wenni-wenti","version":"2022.11.7.0","file":"shena_woyao_zenme_wenni_wenti-2022.11.7.0-py3-none-any.whl","path":"shena-woyao-zenme-wenni-wenti/shena_woyao_zenme_wenni_wenti-2022.11.7.0-py3-none-any.whl"}

---
+++ shena-woyao-zenme-wenni-wenti/shena_woyao_zenme_wenni_wenti-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shena-woyao-zenme-wenni-wenti
+https://github.com/apachecn/shena-woyao-zenme-wenni-wenti"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
