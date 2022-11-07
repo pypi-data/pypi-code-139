7347dd037e6a65561483804407c81dd27920c9cc
Tom Forbes <tom@tomforb.es>
2022-11-07 08:51:29 +0000

    {"name":"qizheng-siyu-yansuan-lijie","version":"2022.11.7.0","file":"qizheng-siyu-yansuan-lijie-2022.11.7.0.tar.gz","path":"qizheng-siyu-yansuan-lijie/qizheng-siyu-yansuan-lijie-2022.11.7.0.tar.gz"}

---
+++ qizheng-siyu-yansuan-lijie/qizheng-siyu-yansuan-lijie-2022.11.7.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import QizhengSiyuYansuanLijie
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('QizhengSiyuYansuanLijie'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="qizheng-siyu-yansuan-lijie",
+    version=QizhengSiyuYansuanLijie.__version__,
+    url="https://github.com/apachecn/qizheng-siyu-yansuan-lijie",
+    author=QizhengSiyuYansuanLijie.__author__,
+    author_email=QizhengSiyuYansuanLijie.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="七政四余演算例解",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "qizheng-siyu-yansuan-lijie=QizhengSiyuYansuanLijie.__main__:main",
+            "QizhengSiyuYansuanLijie=QizhengSiyuYansuanLijie.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
