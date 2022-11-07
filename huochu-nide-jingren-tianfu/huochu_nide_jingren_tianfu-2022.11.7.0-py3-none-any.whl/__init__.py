d30ddca3b2d5f08904eb9345879983b720d5ab10
Tom Forbes <tom@tomforb.es>
2022-11-07 08:32:05 +0000

    {"name":"huochu-nide-jingren-tianfu","version":"2022.11.7.0","file":"huochu_nide_jingren_tianfu-2022.11.7.0-py3-none-any.whl","path":"huochu-nide-jingren-tianfu/huochu_nide_jingren_tianfu-2022.11.7.0-py3-none-any.whl"}

---
+++ huochu-nide-jingren-tianfu/huochu_nide_jingren_tianfu-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huochu-nide-jingren-tianfu
+https://github.com/apachecn/huochu-nide-jingren-tianfu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
