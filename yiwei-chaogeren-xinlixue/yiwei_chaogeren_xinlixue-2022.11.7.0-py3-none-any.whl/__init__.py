7cf873008ce14ce94a83c5851e55783692151e09
Tom Forbes <tom@tomforb.es>
2022-11-07 09:07:01 +0000

    {"name":"yiwei-chaogeren-xinlixue","version":"2022.11.7.0","file":"yiwei_chaogeren_xinlixue-2022.11.7.0-py3-none-any.whl","path":"yiwei-chaogeren-xinlixue/yiwei_chaogeren_xinlixue-2022.11.7.0-py3-none-any.whl"}

---
+++ yiwei-chaogeren-xinlixue/yiwei_chaogeren_xinlixue-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yiwei-chaogeren-xinlixue
+https://github.com/apachecn/yiwei-chaogeren-xinlixue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
