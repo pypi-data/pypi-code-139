71a9e0b86ad3f09239d5ae0de267d65fc2a13615
Tom Forbes <tom@tomforb.es>
2022-11-07 09:03:52 +0000

    {"name":"yide-faze","version":"2022.11.7.0","file":"yide-faze-2022.11.7.0.tar.gz","path":"yide-faze/yide-faze-2022.11.7.0.tar.gz"}

---
+++ yide-faze/yide-faze-2022.11.7.0.tar.gz/YideFaze/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yide-faze
+https://github.com/apachecn/yide-faze"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
