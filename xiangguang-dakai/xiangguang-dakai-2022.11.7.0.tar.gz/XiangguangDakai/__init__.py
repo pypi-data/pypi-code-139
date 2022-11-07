d0860f7b85152545b980572651958d6b1c2fcd32
Tom Forbes <tom@tomforb.es>
2022-11-07 09:00:12 +0000

    {"name":"xiangguang-dakai","version":"2022.11.7.0","file":"xiangguang-dakai-2022.11.7.0.tar.gz","path":"xiangguang-dakai/xiangguang-dakai-2022.11.7.0.tar.gz"}

---
+++ xiangguang-dakai/xiangguang-dakai-2022.11.7.0.tar.gz/XiangguangDakai/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiangguang-dakai
+https://github.com/apachecn/xiangguang-dakai"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
