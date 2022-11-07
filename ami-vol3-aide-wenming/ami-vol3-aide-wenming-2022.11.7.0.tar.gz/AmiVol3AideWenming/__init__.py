b310c137724b4fff4e4dd2a627ee33ed54f58cb5
Tom Forbes <tom@tomforb.es>
2022-11-07 08:19:18 +0000

    {"name":"ami-vol3-aide-wenming","version":"2022.11.7.0","file":"ami-vol3-aide-wenming-2022.11.7.0.tar.gz","path":"ami-vol3-aide-wenming/ami-vol3-aide-wenming-2022.11.7.0.tar.gz"}

---
+++ ami-vol3-aide-wenming/ami-vol3-aide-wenming-2022.11.7.0.tar.gz/AmiVol3AideWenming/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ami-vol3-aide-wenming
+https://github.com/apachecn/ami-vol3-aide-wenming"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
