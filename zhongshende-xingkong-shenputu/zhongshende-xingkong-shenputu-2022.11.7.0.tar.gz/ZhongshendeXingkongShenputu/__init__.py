5ca778bd70e1e79d613892a8bdf6db091b1a9801
Tom Forbes <tom@tomforb.es>
2022-11-07 09:10:41 +0000

    {"name":"zhongshende-xingkong-shenputu","version":"2022.11.7.0","file":"zhongshende-xingkong-shenputu-2022.11.7.0.tar.gz","path":"zhongshende-xingkong-shenputu/zhongshende-xingkong-shenputu-2022.11.7.0.tar.gz"}

---
+++ zhongshende-xingkong-shenputu/zhongshende-xingkong-shenputu-2022.11.7.0.tar.gz/ZhongshendeXingkongShenputu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhongshende-xingkong-shenputu
+https://github.com/apachecn/zhongshende-xingkong-shenputu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
