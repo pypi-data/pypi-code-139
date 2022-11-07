0d2d439b342f8780bf30f1ce5e715d7ab327cef7
Tom Forbes <tom@tomforb.es>
2022-11-07 08:31:56 +0000

    {"name":"huixuan-yuzhou-xuqu","version":"2022.11.7.0","file":"huixuan_yuzhou_xuqu-2022.11.7.0-py3-none-any.whl","path":"huixuan-yuzhou-xuqu/huixuan_yuzhou_xuqu-2022.11.7.0-py3-none-any.whl"}

---
+++ huixuan-yuzhou-xuqu/huixuan_yuzhou_xuqu-2022.11.7.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huixuan-yuzhou-xuqu
+https://github.com/apachecn/huixuan-yuzhou-xuqu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
