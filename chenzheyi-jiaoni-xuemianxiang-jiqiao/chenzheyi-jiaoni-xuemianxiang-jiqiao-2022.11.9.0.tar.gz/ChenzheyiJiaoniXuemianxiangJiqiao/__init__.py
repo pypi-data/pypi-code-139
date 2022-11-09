de37ed0f0931a83b1b841d931cdb1029d74148ac
Tom Forbes <tom@tomforb.es>
2022-11-09 14:44:24 +0000

    {"name":"chenzheyi-jiaoni-xuemianxiang-jiqiao","version":"2022.11.9.0","file":"chenzheyi-jiaoni-xuemianxiang-jiqiao-2022.11.9.0.tar.gz","path":"chenzheyi-jiaoni-xuemianxiang-jiqiao/chenzheyi-jiaoni-xuemianxiang-jiqiao-2022.11.9.0.tar.gz"}

---
+++ chenzheyi-jiaoni-xuemianxiang-jiqiao/chenzheyi-jiaoni-xuemianxiang-jiqiao-2022.11.9.0.tar.gz/ChenzheyiJiaoniXuemianxiangJiqiao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chenzheyi-jiaoni-xuemianxiang-jiqiao
+https://github.com/apachecn/chenzheyi-jiaoni-xuemianxiang-jiqiao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
