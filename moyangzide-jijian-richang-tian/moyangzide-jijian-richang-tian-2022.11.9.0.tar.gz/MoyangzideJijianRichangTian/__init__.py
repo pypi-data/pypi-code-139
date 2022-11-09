dc4a10f808b88e41c8b518a4d72e20322baa35e9
Tom Forbes <tom@tomforb.es>
2022-11-09 14:48:05 +0000

    {"name":"moyangzide-jijian-richang-tian","version":"2022.11.9.0","file":"moyangzide-jijian-richang-tian-2022.11.9.0.tar.gz","path":"moyangzide-jijian-richang-tian/moyangzide-jijian-richang-tian-2022.11.9.0.tar.gz"}

---
+++ moyangzide-jijian-richang-tian/moyangzide-jijian-richang-tian-2022.11.9.0.tar.gz/MoyangzideJijianRichangTian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""moyangzide-jijian-richang-tian
+https://github.com/apachecn/moyangzide-jijian-richang-tian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
