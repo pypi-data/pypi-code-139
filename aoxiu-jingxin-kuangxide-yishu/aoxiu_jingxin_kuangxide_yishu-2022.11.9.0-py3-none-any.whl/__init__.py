329c3710ae1d1eb1e713b6c4f88e71c7e42d35a0
Tom Forbes <tom@tomforb.es>
2022-11-09 14:36:11 +0000

    {"name":"aoxiu-jingxin-kuangxide-yishu","version":"2022.11.9.0","file":"aoxiu_jingxin_kuangxide_yishu-2022.11.9.0-py3-none-any.whl","path":"aoxiu-jingxin-kuangxide-yishu/aoxiu_jingxin_kuangxide_yishu-2022.11.9.0-py3-none-any.whl"}

---
+++ aoxiu-jingxin-kuangxide-yishu/aoxiu_jingxin_kuangxide_yishu-2022.11.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-jingxin-kuangxide-yishu
+https://github.com/apachecn/aoxiu-jingxin-kuangxide-yishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
