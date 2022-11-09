9abc1060325fcb2197b7ca38b1c9e4d1bac75433
Tom Forbes <tom@tomforb.es>
2022-11-09 14:51:34 +0000

    {"name":"shenghuo-lide-fangliao-xiaobaike","version":"2022.11.9.0","file":"shenghuo-lide-fangliao-xiaobaike-2022.11.9.0.tar.gz","path":"shenghuo-lide-fangliao-xiaobaike/shenghuo-lide-fangliao-xiaobaike-2022.11.9.0.tar.gz"}

---
+++ shenghuo-lide-fangliao-xiaobaike/shenghuo-lide-fangliao-xiaobaike-2022.11.9.0.tar.gz/ShenghuoLideFangliaoXiaobaike/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenghuo-lide-fangliao-xiaobaike
+https://github.com/apachecn/shenghuo-lide-fangliao-xiaobaike"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
