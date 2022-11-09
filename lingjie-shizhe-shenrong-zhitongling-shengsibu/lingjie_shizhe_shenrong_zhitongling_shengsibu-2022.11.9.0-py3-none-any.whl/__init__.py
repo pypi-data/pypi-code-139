837fce73e499a8ac4f7c3f23a34620561e60ac2a
Tom Forbes <tom@tomforb.es>
2022-11-09 14:46:08 +0000

    {"name":"lingjie-shizhe-shenrong-zhitongling-shengsibu","version":"2022.11.9.0","file":"lingjie_shizhe_shenrong_zhitongling_shengsibu-2022.11.9.0-py3-none-any.whl","path":"lingjie-shizhe-shenrong-zhitongling-shengsibu/lingjie_shizhe_shenrong_zhitongling_shengsibu-2022.11.9.0-py3-none-any.whl"}

---
+++ lingjie-shizhe-shenrong-zhitongling-shengsibu/lingjie_shizhe_shenrong_zhitongling_shengsibu-2022.11.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingjie-shizhe-shenrong-zhitongling-shengsibu
+https://github.com/apachecn/lingjie-shizhe-shenrong-zhitongling-shengsibu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
