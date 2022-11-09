5ed16aa4c331f180686a5cab3eec7ea3cc950bd8
Tom Forbes <tom@tomforb.es>
2022-11-09 14:49:50 +0000

    {"name":"qihuande-tongling-yinji","version":"2022.11.9.0","file":"qihuande-tongling-yinji-2022.11.9.0.tar.gz","path":"qihuande-tongling-yinji/qihuande-tongling-yinji-2022.11.9.0.tar.gz"}

---
+++ qihuande-tongling-yinji/qihuande-tongling-yinji-2022.11.9.0.tar.gz/QihuandeTonglingYinji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qihuande-tongling-yinji
+https://github.com/apachecn/qihuande-tongling-yinji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
