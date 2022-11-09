a0b4dc8493f55bf509f10c2c805f6a8bb1c97687
Tom Forbes <tom@tomforb.es>
2022-11-09 14:45:18 +0000

    {"name":"gaizao-shengmingde-ziwo-xingxiang-zhengrongshu","version":"2022.11.9.0","file":"gaizao-shengmingde-ziwo-xingxiang-zhengrongshu-2022.11.9.0.tar.gz","path":"gaizao-shengmingde-ziwo-xingxiang-zhengrongshu/gaizao-shengmingde-ziwo-xingxiang-zhengrongshu-2022.11.9.0.tar.gz"}

---
+++ gaizao-shengmingde-ziwo-xingxiang-zhengrongshu/gaizao-shengmingde-ziwo-xingxiang-zhengrongshu-2022.11.9.0.tar.gz/GaizaoShengmingdeZiwoXingxiangZhengrongshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gaizao-shengmingde-ziwo-xingxiang-zhengrongshu
+https://github.com/apachecn/gaizao-shengmingde-ziwo-xingxiang-zhengrongshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
