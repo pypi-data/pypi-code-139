1abba4a70c7ed70fd095e3b3048f73f139dca8c9
Tom Forbes <tom@tomforb.es>
2022-11-09 14:50:24 +0000

    {"name":"renlei-dalishi","version":"2022.11.9.0","file":"renlei_dalishi-2022.11.9.0-py3-none-any.whl","path":"renlei-dalishi/renlei_dalishi-2022.11.9.0-py3-none-any.whl"}

---
+++ renlei-dalishi/renlei_dalishi-2022.11.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""renlei-dalishi
+https://github.com/apachecn/renlei-dalishi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
