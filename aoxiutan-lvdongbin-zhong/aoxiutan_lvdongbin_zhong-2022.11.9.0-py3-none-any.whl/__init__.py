2d8f22bbca6bfa336525c2c147dec8056d41d4cd
Tom Forbes <tom@tomforb.es>
2022-11-09 14:43:06 +0000

    {"name":"aoxiutan-lvdongbin-zhong","version":"2022.11.9.0","file":"aoxiutan_lvdongbin_zhong-2022.11.9.0-py3-none-any.whl","path":"aoxiutan-lvdongbin-zhong/aoxiutan_lvdongbin_zhong-2022.11.9.0-py3-none-any.whl"}

---
+++ aoxiutan-lvdongbin-zhong/aoxiutan_lvdongbin_zhong-2022.11.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiutan-lvdongbin-zhong
+https://github.com/apachecn/aoxiutan-lvdongbin-zhong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
