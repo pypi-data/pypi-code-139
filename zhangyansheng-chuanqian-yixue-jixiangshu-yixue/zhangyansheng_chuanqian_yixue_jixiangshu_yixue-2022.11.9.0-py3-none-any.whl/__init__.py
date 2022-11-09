0599f699e16b29512d177f98ea81d97d5a91fa44
Tom Forbes <tom@tomforb.es>
2022-11-09 14:58:04 +0000

    {"name":"zhangyansheng-chuanqian-yixue-jixiangshu-yixue","version":"2022.11.9.0","file":"zhangyansheng_chuanqian_yixue_jixiangshu_yixue-2022.11.9.0-py3-none-any.whl","path":"zhangyansheng-chuanqian-yixue-jixiangshu-yixue/zhangyansheng_chuanqian_yixue_jixiangshu_yixue-2022.11.9.0-py3-none-any.whl"}

---
+++ zhangyansheng-chuanqian-yixue-jixiangshu-yixue/zhangyansheng_chuanqian_yixue_jixiangshu_yixue-2022.11.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhangyansheng-chuanqian-yixue-jixiangshu-yixue
+https://github.com/apachecn/zhangyansheng-chuanqian-yixue-jixiangshu-yixue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
