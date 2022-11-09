1d82acb6ac1b7b147212b1cba394fa779f5b98d7
Tom Forbes <tom@tomforb.es>
2022-11-09 14:48:53 +0000

    {"name":"nvwu-aede-jujia-xiangcao-baojian-wanyongshu","version":"2022.11.9.0","file":"nvwu_aede_jujia_xiangcao_baojian_wanyongshu-2022.11.9.0-py3-none-any.whl","path":"nvwu-aede-jujia-xiangcao-baojian-wanyongshu/nvwu_aede_jujia_xiangcao_baojian_wanyongshu-2022.11.9.0-py3-none-any.whl"}

---
+++ nvwu-aede-jujia-xiangcao-baojian-wanyongshu/nvwu_aede_jujia_xiangcao_baojian_wanyongshu-2022.11.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nvwu-aede-jujia-xiangcao-baojian-wanyongshu
+https://github.com/apachecn/nvwu-aede-jujia-xiangcao-baojian-wanyongshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
