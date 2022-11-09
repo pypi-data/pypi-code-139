d183ddb31e96ec27b2d567aed6d6ddf0638e1a9b
Tom Forbes <tom@tomforb.es>
2022-11-09 14:56:45 +0000

    {"name":"yiben-dudong-renleitu","version":"2022.11.9.0","file":"yiben-dudong-renleitu-2022.11.9.0.tar.gz","path":"yiben-dudong-renleitu/yiben-dudong-renleitu-2022.11.9.0.tar.gz"}

---
+++ yiben-dudong-renleitu/yiben-dudong-renleitu-2022.11.9.0.tar.gz/YibenDudongRenleitu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yiben-dudong-renleitu
+https://github.com/apachecn/yiben-dudong-renleitu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
