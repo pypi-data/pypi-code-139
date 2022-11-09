c0c06cb4efaa34dcb6b4815d333eae4020a0f9e3
Tom Forbes <tom@tomforb.es>
2022-11-09 14:59:17 +0000

    {"name":"zhuanzhu-yiniande-jingren-liliang","version":"2022.11.9.0","file":"zhuanzhu-yiniande-jingren-liliang-2022.11.9.0.tar.gz","path":"zhuanzhu-yiniande-jingren-liliang/zhuanzhu-yiniande-jingren-liliang-2022.11.9.0.tar.gz"}

---
+++ zhuanzhu-yiniande-jingren-liliang/zhuanzhu-yiniande-jingren-liliang-2022.11.9.0.tar.gz/ZhuanzhuYiniandeJingrenLiliang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhuanzhu-yiniande-jingren-liliang
+https://github.com/apachecn/zhuanzhu-yiniande-jingren-liliang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
