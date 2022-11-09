82b72e98cda316a42c75f8bb108002d957ef547e
Tom Forbes <tom@tomforb.es>
2022-11-09 14:37:20 +0000

    {"name":"aoxiu-shengcun-zhihui-gongzuo-yujinqian","version":"2022.11.9.0","file":"aoxiu_shengcun_zhihui_gongzuo_yujinqian-2022.11.9.0-py3-none-any.whl","path":"aoxiu-shengcun-zhihui-gongzuo-yujinqian/aoxiu_shengcun_zhihui_gongzuo_yujinqian-2022.11.9.0-py3-none-any.whl"}

---
+++ aoxiu-shengcun-zhihui-gongzuo-yujinqian/aoxiu_shengcun_zhihui_gongzuo_yujinqian-2022.11.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-shengcun-zhihui-gongzuo-yujinqian
+https://github.com/apachecn/aoxiu-shengcun-zhihui-gongzuo-yujinqian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
