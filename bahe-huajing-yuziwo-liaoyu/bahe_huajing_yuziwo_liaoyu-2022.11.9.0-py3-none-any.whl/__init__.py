a968f7432f6a613372459ac25d66b86a2ecf009d
Tom Forbes <tom@tomforb.es>
2022-11-09 14:43:25 +0000

    {"name":"bahe-huajing-yuziwo-liaoyu","version":"2022.11.9.0","file":"bahe_huajing_yuziwo_liaoyu-2022.11.9.0-py3-none-any.whl","path":"bahe-huajing-yuziwo-liaoyu/bahe_huajing_yuziwo_liaoyu-2022.11.9.0-py3-none-any.whl"}

---
+++ bahe-huajing-yuziwo-liaoyu/bahe_huajing_yuziwo_liaoyu-2022.11.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bahe-huajing-yuziwo-liaoyu
+https://github.com/apachecn/bahe-huajing-yuziwo-liaoyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
