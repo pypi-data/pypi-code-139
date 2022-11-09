1ad400468f7584a2a63cffa10912f9a3f5fd0f67
Tom Forbes <tom@tomforb.es>
2022-11-09 14:45:32 +0000

    {"name":"huidao-nide-neizai-quanwei","version":"2022.11.9.0","file":"huidao-nide-neizai-quanwei-2022.11.9.0.tar.gz","path":"huidao-nide-neizai-quanwei/huidao-nide-neizai-quanwei-2022.11.9.0.tar.gz"}

---
+++ huidao-nide-neizai-quanwei/huidao-nide-neizai-quanwei-2022.11.9.0.tar.gz/HuidaoNideNeizaiQuanwei/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huidao-nide-neizai-quanwei
+https://github.com/apachecn/huidao-nide-neizai-quanwei"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
