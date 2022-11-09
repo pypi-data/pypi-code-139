39224325dbde4177f0cda7cb74d0139136ca5f38
Tom Forbes <tom@tomforb.es>
2022-11-09 14:34:10 +0000

    {"name":"aoxiu-daoda-zhenaide-lvcheng","version":"2022.11.9.0","file":"aoxiu-daoda-zhenaide-lvcheng-2022.11.9.0.tar.gz","path":"aoxiu-daoda-zhenaide-lvcheng/aoxiu-daoda-zhenaide-lvcheng-2022.11.9.0.tar.gz"}

---
+++ aoxiu-daoda-zhenaide-lvcheng/aoxiu-daoda-zhenaide-lvcheng-2022.11.9.0.tar.gz/AoxiuDaodaZhenaideLvcheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-daoda-zhenaide-lvcheng
+https://github.com/apachecn/aoxiu-daoda-zhenaide-lvcheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
