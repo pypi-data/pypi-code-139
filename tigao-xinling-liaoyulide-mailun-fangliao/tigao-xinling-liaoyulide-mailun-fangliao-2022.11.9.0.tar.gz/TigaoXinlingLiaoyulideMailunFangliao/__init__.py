434d9c118d52ef92c9556aa9286db416e4b2bb2b
Tom Forbes <tom@tomforb.es>
2022-11-09 14:52:03 +0000

    {"name":"tigao-xinling-liaoyulide-mailun-fangliao","version":"2022.11.9.0","file":"tigao-xinling-liaoyulide-mailun-fangliao-2022.11.9.0.tar.gz","path":"tigao-xinling-liaoyulide-mailun-fangliao/tigao-xinling-liaoyulide-mailun-fangliao-2022.11.9.0.tar.gz"}

---
+++ tigao-xinling-liaoyulide-mailun-fangliao/tigao-xinling-liaoyulide-mailun-fangliao-2022.11.9.0.tar.gz/TigaoXinlingLiaoyulideMailunFangliao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tigao-xinling-liaoyulide-mailun-fangliao
+https://github.com/apachecn/tigao-xinling-liaoyulide-mailun-fangliao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
