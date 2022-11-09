9783e7a514cf5f99383b24293dd337cce13cc164
Tom Forbes <tom@tomforb.es>
2022-11-09 14:31:36 +0000

    {"name":"aoxiu-aoiuxiu-tanyongqi","version":"2022.11.9.0","file":"aoxiu_aoiuxiu_tanyongqi-2022.11.9.0-py3-none-any.whl","path":"aoxiu-aoiuxiu-tanyongqi/aoxiu_aoiuxiu_tanyongqi-2022.11.9.0-py3-none-any.whl"}

---
+++ aoxiu-aoiuxiu-tanyongqi/aoxiu_aoiuxiu_tanyongqi-2022.11.9.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-aoiuxiu-tanyongqi
+https://github.com/apachecn/aoxiu-aoiuxiu-tanyongqi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.9.0"
