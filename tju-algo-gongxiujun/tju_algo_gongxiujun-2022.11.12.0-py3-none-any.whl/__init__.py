77211c8f1e8819ded2bcf474b74a45df8550ddec
Tom Forbes <tom@tomforb.es>
2022-11-12 11:11:20 +0000

    {"name":"tju-algo-gongxiujun","version":"2022.11.12.0","file":"tju_algo_gongxiujun-2022.11.12.0-py3-none-any.whl","path":"tju-algo-gongxiujun/tju_algo_gongxiujun-2022.11.12.0-py3-none-any.whl"}

---
+++ tju-algo-gongxiujun/tju_algo_gongxiujun-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tju-algo-gongxiujun
+https://github.com/apachecn/tju-algo-gongxiujun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
