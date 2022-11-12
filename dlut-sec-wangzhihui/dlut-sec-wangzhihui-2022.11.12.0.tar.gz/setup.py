bd85be1495c38e35179ec95924e31365419ed444
Tom Forbes <tom@tomforb.es>
2022-11-12 10:46:17 +0000

    {"name":"dlut-sec-wangzhihui","version":"2022.11.12.0","file":"dlut-sec-wangzhihui-2022.11.12.0.tar.gz","path":"dlut-sec-wangzhihui/dlut-sec-wangzhihui-2022.11.12.0.tar.gz"}

---
+++ dlut-sec-wangzhihui/dlut-sec-wangzhihui-2022.11.12.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import DlutSecWangzhihui
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('DlutSecWangzhihui'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="dlut-sec-wangzhihui",
+    version=DlutSecWangzhihui.__version__,
+    url="https://github.com/apachecn/dlut-sec-wangzhihui",
+    author=DlutSecWangzhihui.__author__,
+    author_email=DlutSecWangzhihui.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="大连理工网络安全课件（王智慧）",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "dlut-sec-wangzhihui=DlutSecWangzhihui.__main__:main",
+            "DlutSecWangzhihui=DlutSecWangzhihui.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
