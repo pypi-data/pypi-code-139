b3e72ec7d6641cf7ade2b4d64e04383ea6099941
Tom Forbes <tom@tomforb.es>
2022-11-12 10:36:13 +0000

    {"name":"dlut-cpp","version":"2022.11.12.0","file":"dlut-cpp-2022.11.12.0.tar.gz","path":"dlut-cpp/dlut-cpp-2022.11.12.0.tar.gz"}

---
+++ dlut-cpp/dlut-cpp-2022.11.12.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import DlutCpp
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('DlutCpp'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="dlut-cpp",
+    version=DlutCpp.__version__,
+    url="https://github.com/apachecn/dlut-cpp",
+    author=DlutCpp.__author__,
+    author_email=DlutCpp.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="大连理工C++课件",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "dlut-cpp=DlutCpp.__main__:main",
+            "DlutCpp=DlutCpp.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
