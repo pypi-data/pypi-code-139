531c6497130e80c139de14f72f21a3831397fa1f
Tom Forbes <tom@tomforb.es>
2022-11-12 10:48:54 +0000

    {"name":"hit-comp-theory","version":"2022.11.12.0","file":"hit_comp_theory-2022.11.12.0-py3-none-any.whl","path":"hit-comp-theory/hit_comp_theory-2022.11.12.0-py3-none-any.whl"}

---
+++ hit-comp-theory/hit_comp_theory-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hit-comp-theory
+https://github.com/apachecn/hit-comp-theory"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
