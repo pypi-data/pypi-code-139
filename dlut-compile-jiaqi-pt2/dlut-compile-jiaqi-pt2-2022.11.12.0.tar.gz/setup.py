ed24435bed129a64cd148cebffbedb642fd55e2a
Tom Forbes <tom@tomforb.es>
2022-11-12 10:35:59 +0000

    {"name":"dlut-compile-jiaqi-pt2","version":"2022.11.12.0","file":"dlut-compile-jiaqi-pt2-2022.11.12.0.tar.gz","path":"dlut-compile-jiaqi-pt2/dlut-compile-jiaqi-pt2-2022.11.12.0.tar.gz"}

---
+++ dlut-compile-jiaqi-pt2/dlut-compile-jiaqi-pt2-2022.11.12.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import DlutCompileJiaqiPt2
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('DlutCompileJiaqiPt2'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="dlut-compile-jiaqi-pt2",
+    version=DlutCompileJiaqiPt2.__version__,
+    url="https://github.com/apachecn/dlut-compile-jiaqi-pt2",
+    author=DlutCompileJiaqiPt2.__author__,
+    author_email=DlutCompileJiaqiPt2.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="大连理工编译原理课件（贾棋）（二）",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "dlut-compile-jiaqi-pt2=DlutCompileJiaqiPt2.__main__:main",
+            "DlutCompileJiaqiPt2=DlutCompileJiaqiPt2.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
