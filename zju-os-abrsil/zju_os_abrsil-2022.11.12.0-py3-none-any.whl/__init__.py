836936b8b2fcb515475c204e49c5ea4a419da726
Tom Forbes <tom@tomforb.es>
2022-11-12 11:17:35 +0000

    {"name":"zju-os-abrsil","version":"2022.11.12.0","file":"zju_os_abrsil-2022.11.12.0-py3-none-any.whl","path":"zju-os-abrsil/zju_os_abrsil-2022.11.12.0-py3-none-any.whl"}

---
+++ zju-os-abrsil/zju_os_abrsil-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zju-os-abrsil
+https://github.com/apachecn/zju-os-abrsil"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
