92f1fdfdd27b8ffd934922909f584ec3140ed984
Tom Forbes <tom@tomforb.es>
2022-11-12 10:52:59 +0000

    {"name":"hit-proj-mgt","version":"2022.11.12.0","file":"hit-proj-mgt-2022.11.12.0.tar.gz","path":"hit-proj-mgt/hit-proj-mgt-2022.11.12.0.tar.gz"}

---
+++ hit-proj-mgt/hit-proj-mgt-2022.11.12.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import HitProjMgt
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('HitProjMgt'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="hit-proj-mgt",
+    version=HitProjMgt.__version__,
+    url="https://github.com/apachecn/hit-proj-mgt",
+    author=HitProjMgt.__author__,
+    author_email=HitProjMgt.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="哈工大项目管理课件",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "hit-proj-mgt=HitProjMgt.__main__:main",
+            "HitProjMgt=HitProjMgt.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
