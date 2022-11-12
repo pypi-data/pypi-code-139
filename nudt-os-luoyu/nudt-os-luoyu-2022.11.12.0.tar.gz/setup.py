5b8eae8eb18b21d9d8852537f2811f8b07a59d3e
Tom Forbes <tom@tomforb.es>
2022-11-12 11:10:10 +0000

    {"name":"nudt-os-luoyu","version":"2022.11.12.0","file":"nudt-os-luoyu-2022.11.12.0.tar.gz","path":"nudt-os-luoyu/nudt-os-luoyu-2022.11.12.0.tar.gz"}

---
+++ nudt-os-luoyu/nudt-os-luoyu-2022.11.12.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import NudtOsLuoyu
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('NudtOsLuoyu'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="nudt-os-luoyu",
+    version=NudtOsLuoyu.__version__,
+    url="https://github.com/apachecn/nudt-os-luoyu",
+    author=NudtOsLuoyu.__author__,
+    author_email=NudtOsLuoyu.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="国防科大操作系统课件（罗宇）",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "nudt-os-luoyu=NudtOsLuoyu.__main__:main",
+            "NudtOsLuoyu=NudtOsLuoyu.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
