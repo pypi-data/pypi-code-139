3c989b82511f3b4f93099a622e256b38e37b3ff3
Tom Forbes <tom@tomforb.es>
2022-11-12 11:08:30 +0000

    {"name":"nudt-comp-arch","version":"2022.11.12.0","file":"nudt-comp-arch-2022.11.12.0.tar.gz","path":"nudt-comp-arch/nudt-comp-arch-2022.11.12.0.tar.gz"}

---
+++ nudt-comp-arch/nudt-comp-arch-2022.11.12.0.tar.gz/NudtCompArch/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nudt-comp-arch
+https://github.com/apachecn/nudt-comp-arch"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
