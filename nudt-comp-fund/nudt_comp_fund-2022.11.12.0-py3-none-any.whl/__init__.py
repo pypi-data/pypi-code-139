fb300564ae1adcb97645568089f8c5f69b384663
Tom Forbes <tom@tomforb.es>
2022-11-12 11:08:44 +0000

    {"name":"nudt-comp-fund","version":"2022.11.12.0","file":"nudt_comp_fund-2022.11.12.0-py3-none-any.whl","path":"nudt-comp-fund/nudt_comp_fund-2022.11.12.0-py3-none-any.whl"}

---
+++ nudt-comp-fund/nudt_comp_fund-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nudt-comp-fund
+https://github.com/apachecn/nudt-comp-fund"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
