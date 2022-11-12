f0bafcd0e8452ee7da764fae3f7afa5987af6d85
Tom Forbes <tom@tomforb.es>
2022-11-12 10:18:12 +0000

    {"name":"buaa-soft-arch-zhangli","version":"2022.11.12.0","file":"buaa_soft_arch_zhangli-2022.11.12.0-py3-none-any.whl","path":"buaa-soft-arch-zhangli/buaa_soft_arch_zhangli-2022.11.12.0-py3-none-any.whl"}

---
+++ buaa-soft-arch-zhangli/buaa_soft_arch_zhangli-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""buaa-soft-arch-zhangli
+https://github.com/apachecn/buaa-soft-arch-zhangli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
