7bd6935e1037a0b67573b74b89d5c1f4b413366a
Tom Forbes <tom@tomforb.es>
2022-11-12 10:52:24 +0000

    {"name":"hit-net-nielanshun","version":"2022.11.12.0","file":"hit_net_nielanshun-2022.11.12.0-py3-none-any.whl","path":"hit-net-nielanshun/hit_net_nielanshun-2022.11.12.0-py3-none-any.whl"}

---
+++ hit-net-nielanshun/hit_net_nielanshun-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hit-net-nielanshun
+https://github.com/apachecn/hit-net-nielanshun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
