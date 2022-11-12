22d3acb57f79c582910fc908ab9f6d21c83e477b
Tom Forbes <tom@tomforb.es>
2022-11-12 10:29:27 +0000

    {"name":"bupt-db-xiaoda","version":"2022.11.12.0","file":"bupt_db_xiaoda-2022.11.12.0-py3-none-any.whl","path":"bupt-db-xiaoda/bupt_db_xiaoda-2022.11.12.0-py3-none-any.whl"}

---
+++ bupt-db-xiaoda/bupt_db_xiaoda-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bupt-db-xiaoda
+https://github.com/apachecn/bupt-db-xiaoda"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
