08cd9ad1b2ee900488ff756ef97f08c233e69f93
Tom Forbes <tom@tomforb.es>
2022-11-12 11:09:43 +0000

    {"name":"nudt-oop-soft-engi-dongwei","version":"2022.11.12.0","file":"nudt_oop_soft_engi_dongwei-2022.11.12.0-py3-none-any.whl","path":"nudt-oop-soft-engi-dongwei/nudt_oop_soft_engi_dongwei-2022.11.12.0-py3-none-any.whl"}

---
+++ nudt-oop-soft-engi-dongwei/nudt_oop_soft_engi_dongwei-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nudt-oop-soft-engi-dongwei
+https://github.com/apachecn/nudt-oop-soft-engi-dongwei"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
