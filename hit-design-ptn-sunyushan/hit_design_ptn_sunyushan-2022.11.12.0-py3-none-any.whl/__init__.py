ebcde382130f5b6176980741f764f1e2f1b35cd8
Tom Forbes <tom@tomforb.es>
2022-11-12 10:50:22 +0000

    {"name":"hit-design-ptn-sunyushan","version":"2022.11.12.0","file":"hit_design_ptn_sunyushan-2022.11.12.0-py3-none-any.whl","path":"hit-design-ptn-sunyushan/hit_design_ptn_sunyushan-2022.11.12.0-py3-none-any.whl"}

---
+++ hit-design-ptn-sunyushan/hit_design_ptn_sunyushan-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hit-design-ptn-sunyushan
+https://github.com/apachecn/hit-design-ptn-sunyushan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
