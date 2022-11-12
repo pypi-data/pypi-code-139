ff6ed9e1c2f62a44fb4195ad88b9c85de92f9314
Tom Forbes <tom@tomforb.es>
2022-11-12 10:08:22 +0000

    {"name":"buaa-crypto-liweiguo","version":"2022.11.12.0","file":"buaa_crypto_liweiguo-2022.11.12.0-py3-none-any.whl","path":"buaa-crypto-liweiguo/buaa_crypto_liweiguo-2022.11.12.0-py3-none-any.whl"}

---
+++ buaa-crypto-liweiguo/buaa_crypto_liweiguo-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""buaa-crypto-liweiguo
+https://github.com/apachecn/buaa-crypto-liweiguo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
