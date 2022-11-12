5b0bb627cc72ffbdda09c37a8aef91db5591ed72
Tom Forbes <tom@tomforb.es>
2022-11-12 10:11:25 +0000

    {"name":"buaa-net-liuyi","version":"2022.11.12.0","file":"buaa_net_liuyi-2022.11.12.0-py3-none-any.whl","path":"buaa-net-liuyi/buaa_net_liuyi-2022.11.12.0-py3-none-any.whl"}

---
+++ buaa-net-liuyi/buaa_net_liuyi-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""buaa-net-liuyi
+https://github.com/apachecn/buaa-net-liuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
