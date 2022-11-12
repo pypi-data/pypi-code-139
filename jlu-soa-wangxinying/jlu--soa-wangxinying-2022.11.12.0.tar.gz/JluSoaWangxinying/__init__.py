99f6ae3d1f45f7583b0cee52feafaad3a3713ec9
Tom Forbes <tom@tomforb.es>
2022-11-12 10:54:08 +0000

    {"name":"jlu-soa-wangxinying","version":"2022.11.12.0","file":"jlu--soa-wangxinying-2022.11.12.0.tar.gz","path":"jlu-soa-wangxinying/jlu--soa-wangxinying-2022.11.12.0.tar.gz"}

---
+++ jlu-soa-wangxinying/jlu--soa-wangxinying-2022.11.12.0.tar.gz/JluSoaWangxinying/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jlu--soa-wangxinying
+https://github.com/apachecn/jlu--soa-wangxinying"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
