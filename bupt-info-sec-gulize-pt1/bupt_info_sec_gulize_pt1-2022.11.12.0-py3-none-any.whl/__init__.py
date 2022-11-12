eefa6b42ea90b1005299847571d4c51fefdd43dd
Tom Forbes <tom@tomforb.es>
2022-11-12 10:31:14 +0000

    {"name":"bupt-info-sec-gulize-pt1","version":"2022.11.12.0","file":"bupt_info_sec_gulize_pt1-2022.11.12.0-py3-none-any.whl","path":"bupt-info-sec-gulize-pt1/bupt_info_sec_gulize_pt1-2022.11.12.0-py3-none-any.whl"}

---
+++ bupt-info-sec-gulize-pt1/bupt_info_sec_gulize_pt1-2022.11.12.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bupt-info-sec-gulize-pt1
+https://github.com/apachecn/bupt-info-sec-gulize-pt1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
