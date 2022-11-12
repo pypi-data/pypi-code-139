5fc1cb55806e7622da78ad5cffa14a87f409ab50
Tom Forbes <tom@tomforb.es>
2022-11-12 11:17:11 +0000

    {"name":"zju-java-luhanquan","version":"2022.11.12.0","file":"zju-java-luhanquan-2022.11.12.0.tar.gz","path":"zju-java-luhanquan/zju-java-luhanquan-2022.11.12.0.tar.gz"}

---
+++ zju-java-luhanquan/zju-java-luhanquan-2022.11.12.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ZjuJavaLuhanquan
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ZjuJavaLuhanquan'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="zju-java-luhanquan",
+    version=ZjuJavaLuhanquan.__version__,
+    url="https://github.com/apachecn/zju-java-luhanquan",
+    author=ZjuJavaLuhanquan.__author__,
+    author_email=ZjuJavaLuhanquan.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="浙江大学Java课件（陆汉权）",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "zju-java-luhanquan=ZjuJavaLuhanquan.__main__:main",
+            "ZjuJavaLuhanquan=ZjuJavaLuhanquan.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
