484fa2e7de6913f33a2f5f3f98af02ed478b0e3b
Tom Forbes <tom@tomforb.es>
2022-11-12 10:46:06 +0000

    {"name":"dlut-sec-hanyu","version":"2022.11.12.0","file":"dlut-sec-hanyu-2022.11.12.0.tar.gz","path":"dlut-sec-hanyu/dlut-sec-hanyu-2022.11.12.0.tar.gz"}

---
+++ dlut-sec-hanyu/dlut-sec-hanyu-2022.11.12.0.tar.gz/DlutSecHanyu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dlut-sec-hanyu
+https://github.com/apachecn/dlut-sec-hanyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.12.0"
